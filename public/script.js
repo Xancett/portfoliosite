d90fa28dec9c7739a7abe927c2891000bc39efa0
Spiky <ryan.j@epoxstudios.com>
2020-02-19 12:08:15 -0500

    Button setup WIP

--- public/script.js
+++ public/script.js
@@ -6,10 +6,10 @@ for (var i = 0; i < colButton.length; i++) {
     colButton[i].addEventListener("click", function () {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
-        if (content.style.display === "block") {
+        if (content.style.display === "table") {
             content.style.display = "none";
         } else {
-            content.style.display = "block";
+            content.style.display = "table";
         }
     })
 }
